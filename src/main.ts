f362f9475a20ea5d5db225cc7e646b364c606fd6
Leozhu123 <570727732@qq.com>
2020-07-14 20:40:34 +0800

    harvester+upgrader的简陋逻辑

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,34 @@
 import { ErrorMapper } from "utils/ErrorMapper";
-
+import { Harvester } from "role/harvester";
+import { Upgrader } from "role/upgrader";
 // When compiling TS to JS and bundling with rollup, the line numbers and file names in error messages change
 // This utility uses source maps to get the line numbers and file names of the original, TS source code
+const roomNameList = ['E38N43'];
 export const loop = ErrorMapper.wrapLoop(() => {
-  console.log(`Current game tick is ${Game.time}`);
-
-  // Automatically delete memory of missing creeps
+  /*   for (const roomName in roomNameList) {
+      if(Game.rooms[roomName]){
+        let room = Game.rooms[roomName];
+        for(const creepName in room.cr)
+      }
+    } */
+  let cnt = 0;
+  for (const creepName in Game.creeps) {
+    const creep = Game.creeps[creepName];
+    if (creep.memory.role === 'harvester')
+      Harvester.run(creep);
+    if (creep.memory.role === 'upgrader')
+      Upgrader.run(creep);
+    cnt++;
+  }
+  console.log(cnt);
+  const spawnName = 'Spawn1';
+  if (Game.spawns[spawnName]) {
+    if (cnt <= 5) {
+      Game.spawns[spawnName].spawnCreep([MOVE, MOVE, CARRY, CARRY, WORK], 'harvester' + Game.time, { memory: { role: 'harvester', room: 'E38N43', working: false } });
+    } else if (cnt <= 7) {
+      Game.spawns[spawnName].spawnCreep([MOVE, MOVE, CARRY, CARRY, WORK], 'upgrader' + Game.time, { memory: { role: 'upgrader', room: 'E38N43', working: false } });
+    }
+  }
   for (const name in Memory.creeps) {
     if (!(name in Game.creeps)) {
       delete Memory.creeps[name];
